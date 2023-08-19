b27e9d022e4268160b3541c4aa59c77c9ea438ac
tachyonok <tachyonparticle123@gmail.com>
2023-08-19 11:29:31 +0530

    use of body parser for parsing data via form fields

--- app.js
+++ app.js
@@ -1,56 +1,75 @@
 const express = require("express");
 const https = require("https");
 
+const bodyParser = require("body-parser");
+
 const app =  express();
 
+//Neccasory code to get access the body
+
+app.use(bodyParser.urlencoded({extended:true})
+    );
+
 app.get("/",(req,res)=>{
-
-    // we can only have one res.send
-
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=334579555c16d8f383591da138e15101&units=metric";
-
-    // making http get request to the browser to fetch data from an api endpoint
-
-    https.get(url ,(response) => {
-        console.log(response.statusCode)
-
-        response.on('data',(data)=>{
-            // we do this since the data coming from the api is in hex values
-
-            const WeatherData=JSON.parse(data);
-
-            const Alphabetdata = JSON.stringify(data);
-
-            const object = {
-                name:"harish",
-                bfff:"ridhhi"
-            }
-
-            //gettting individual data from an api
-
-            const temp  = WeatherData.main.temp;
-            const feels = WeatherData.main.feels_like;
-            const dsp = WeatherData.weather[0].description;
-            const icon = WeatherData.weather[0].icon;
-            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-
-            console.log(dsp , feels);
-           
-            res.write("<h1>Temperature in london is " + temp  + "C</h1>");
-            res.write("<p>The weather is currently " + dsp + "</p>");
-            res.write("<img src = " + imageURL + ">");
-
-            res.send();
-
-       //it will convert the object into a string
-
-            console.log(JSON.stringify(object));
-            
-            console.log(weatherimage)
-        })
-    })
-   
+    res.sendFile(__dirname + "/index.html")
+ 
 })
+
+app.post("/",(req,res)=>{
+
+    console.log(req.body.cityName);
+     const city = req.body.cityName;
+const apikey = "334579555c16d8f383591da138e15101";
+const unit = "metric";
+
+// we can only have one res.send
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=${unit}`;
+
+// making http get request to the browser to fetch data from an api endpoint
+
+https.get(url ,(response) => {
+    console.log(response.statusCode)
+
+    response.on('data',(data)=>{
+        // we do this since the data coming from the api is in hex values
+
+        const WeatherData=JSON.parse(data);
+
+        const Alphabetdata = JSON.stringify(data);
+
+        const object = {
+            name:"harish",
+            bfff:"ridhhi"
+        }
+
+        //gettting individual data from an api
+
+        const temp  = WeatherData.main.temp;
+        const feels = WeatherData.main.feels_like;
+        const dsp = WeatherData.weather[0].description;
+        const icon = WeatherData.weather[0].icon;
+        const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+
+        console.log(dsp , feels);
+       
+        res.write("<h1>Temperature in " + city + " is "  + temp  + "C</h1>");
+        res.write("<p>The weather is currently " + dsp + "</p>");
+        res.write("<img src = " + imageURL + ">");
+
+        res.send();
+
+   //it will convert the object into a string
+
+        console.log(JSON.stringify(object));
+        
+        console.log(icon)
+    })
+})
+
+})
+
+
 
 app.listen(3000,()=>{
     console.log("server stared on 3000");
